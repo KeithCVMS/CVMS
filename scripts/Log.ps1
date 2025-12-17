c12e681ef8b2856a18f2c7b127b1d162abbd9d39
KeithCVMS <116287511+KeithCVMS@users.noreply.github.com>
2025-12-16 17:42:42 -0700

    Add files via upload

--- scripts/Log.ps1
+++ scripts/Log.ps1
@@ -1,10 +1,12 @@
-	Function Log() {
-		[CmdletBinding()]
-		param (
-			[Parameter(Mandatory=$false)] [String] $message
-		)
+$sw = [System.Diagnostics.Stopwatch]::StartNew()
 
-		$tz = [Regex]::Replace([System.TimeZoneInfo]::Local.StandardName, '([A-Z])\w+\s*', '$1')
-		$ts = get-date -f "yyyy/MM/dd hh:mm:ss tt"
-		if ($message) { Write-Output "$ts $tz -  $message"} else {write-output ""}
-	}
+Function Log() {
+	[CmdletBinding()]
+	param ( [Parameter(Mandatory=$false)] [String] $message )
+
+	$ts = get-date -f "yyyy/MM/dd hh:mm:ss tt"
+	$tz = [Regex]::Replace([System.TimeZoneInfo]::Local.StandardName, '([A-Z])\w+\s*', '$1')
+	$et = " TET: $($sw.Elapsed.ToString('hh\:mm\:ss'))"
+	$fts = "$ts $tz "
+	if ($message) { Write-Output "$($ts) $($tz): $message @$et"} else {write-output ""}
+}
